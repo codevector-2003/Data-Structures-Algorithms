ddacd5845af46d3e0ec74802485c6a029dade1e6
Haren Daishika <samprasharendaishika@gmail.com>
2025-05-23 00:02:00 +0530

    Added Graph class

---
+++ Runner_Graph.java
@@ -0,0 +1,38 @@
+import java.util.Vector;
+
+class Graph {
+    Vector<Vector<Integer>> adj;
+
+    public Graph(int vertices) {
+        adj = new Vector<Vector<Integer>>();
+        for (int i = 0; i < vertices; i++) {
+            adj.add(new Vector<Integer>());
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < adj.size(); i++) {
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class Runner_Graph {
+    public static void main(String args[]) {
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.printGraph();
+    }
+
+}
