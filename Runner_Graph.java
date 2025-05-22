f40fd8e3c43caa53de5ca670aa8b3f2aeed6edf3
Haren Daishika <samprasharendaishika@gmail.com>
2025-05-23 00:14:07 +0530

    Implement Graph data structure with Directed and Undirected graph classes
    - Created abstract Graph class with addEdge and printGraph methods
    - Implemented Undirected_Graph class with bidirectional edge support
    - Implemented Directed_Graph class with single direction edge support
    - Added test cases in Runner_Graph to demonstrate functionality

--- Runner_Graph.java
+++ Runner_Graph.java
@@ -1,15 +1,22 @@
 import java.util.Vector;
 
-class Graph {
+abstract class Graph {
+    abstract void addEdge(int u, int v);
+
+    abstract void printGraph();
+}
+
+class Undirected_Graph extends Graph {
     Vector<Vector<Integer>> adj;
 
-    public Graph(int vertices) {
+    public Undirected_Graph(int vertices) {
         adj = new Vector<Vector<Integer>>();
         for (int i = 0; i < vertices; i++) {
             adj.add(new Vector<Integer>());
         }
     }
 
+    @Override
     public void addEdge(int u, int v) {
         adj.get(u).add(v);
         adj.get(v).add(u);
@@ -25,9 +32,35 @@ class Graph {
     }
 }
 
+class Directed_Graph extends Graph {
+    Vector<Vector<Integer>> adj;
+
+    public Directed_Graph(int vertices) {
+        adj = new Vector<Vector<Integer>>();
+        for (int i = 0; i < vertices; i++) {
+            adj.add(new Vector<Integer>());
+        }
+    }
+
+    @Override
+    public void addEdge(int u, int v) {
+        adj.get(u).add(v);
+    }
+
+    @Override
+    public void printGraph() {
+        for (int i = 0; i < adj.size(); i++) {
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 public class Runner_Graph {
     public static void main(String args[]) {
-        Graph g = new Graph(4);
+        Undirected_Graph g = new Undirected_Graph(4);
         g.addEdge(0, 1);
         g.addEdge(0, 2);
         g.addEdge(1, 2);
